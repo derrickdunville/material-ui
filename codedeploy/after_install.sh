be1ff7047b629303a6e8a564d699fe851ca2222d
Derrick Dunville <derrickdunville@gmail.com>
2019-06-08 16:31:43 -0400

    now calling aws directly.

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -27,20 +27,25 @@ export DISCORD_GUILD_ID=353342769630281738
 export DISCORD_WELCOME_CHANNEL_ID=553815971597516800
 
 # configuring secret ENV VARS on the instance using AWS Parameter Store
-stripe_publishable_key=$(aws ssm get-parameters --region us-east-1 --names prodMaterialUiStripePublishableKey --with-decryption --query Parameters[0].Value)
+stripe_publishable_key=$(~/.local/bin/aws ssm get-parameters --region us-east-1 --names prodMaterialUiStripePublishableKey --with-decryption --query Parameters[0].Value)
 export STRIPE_PUBLISHABLE_KEY=$stripe_publishable_key
 echo $stripe_publishable_key
 
-discord_client_secret=$(aws ssm get-parameters --region us-east-1 --names prodMaterialUiDiscordClientSecret --with-decryption --query Parameters[0].Value)
+discord_client_secret=$(~/.local/bin/aws ssm get-parameters --region us-east-1 --names prodMaterialUiDiscordClientSecret --with-decryption --query Parameters[0].Value)
 export DISCORD_CLIENT_SECRET=$discord_client_secret
 echo $discord_client_secret
 
-recaptcha_site_key=$(aws ssm get-parameters --region us-east-1 --names prodMaterialUiRecaptchaSiteKey --with-decryption --query Parameters[0].Value)
+recaptcha_site_key=$(~/.local/bin/aws ssm get-parameters --region us-east-1 --names prodMaterialUiRecaptchaSiteKey --with-decryption --query Parameters[0].Value)
 export RECAPTCHA_SITE_KEY=$recaptcha_site_key
 echo recaptcha_site_key
 
-echo NODE_ENV
-
+echo "NODE_ENV: $NODE_ENV"
+echo "API_URL: $API_URL"
+echo "CLIENT_ROOT: $CLIENT_ROOT"
+echo "DISCORD_CLIENT_ID: $DISCORD_CLIENT_ID"
+echo "DISCORD_CALLBACK: $DISCORD_CALLBACK"
+echo "DISCORD_GUILD_ID: $DISCORD_GUILD_ID"
+echo "DISCORD_WELCOME_CHANNEL_ID: $DISCORD_WELCOME_CHANNEL_ID"
 
 echo building application...
 npm run codedeploy:production:build-server
