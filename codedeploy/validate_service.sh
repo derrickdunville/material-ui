79ee9964d6fc9b1580f601c268289da4540d973f
Derrick Dunville <derrickdunville@gmail.com>
2019-06-08 13:10:01 -0400

    validate service with curl. We grab the http resonse code, check if it is 200 and exit successfully, else we exit with failure.

--- codedeploy/validate_service.sh
+++ codedeploy/validate_service.sh
@@ -5,5 +5,18 @@
 # verify the deployment was completed successfully.
 
 # for now we are just checking that we get back a page from the server
-echo validating service...
-#curl -m 5 https://localhost:80
+echo validating service...;
+
+# we use curl to test that the server is responding with a 200 HTTP status code
+# its necessary to use -k --insecure when testing localhost over https since the
+# certificate cannot be validated against localhost
+HTTP_STATUS="$(curl --write-out %{http_code} --silent --output /dev/null -k --insecure https://localhost)"
+if [ $HTTP_STATUS = "200" ]; then
+        echo "Server responded with 200. All is well";
+        echo "service validated successfully"
+        exit 0; #successfully exit
+else
+        echo "Server responded with wrong http response code";
+        echo "service validation failed"
+        exit 1; #exit with failure:
+fi;
